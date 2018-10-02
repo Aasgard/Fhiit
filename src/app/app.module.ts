a12c2f4a85ed5af07f2c3b677aa760c0c496276d
François-Régis LANCIEN <francois.regis.lancien@gmail.com>
2018-10-02 17:23:22 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,44 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {MyApp} from './app.component';
+import {Camera} from '@ionic-native/camera';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import {StatusBar} from '@ionic-native/status-bar';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {ExercisesPage} from "../pages/exercises/exercises";
+import {WorkoutsPage} from "../pages/workouts/workouts";
+import {InfosPage} from "../pages/infos/infos";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        WorkoutsPage,
+        ExercisesPage,
+        InfosPage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        WorkoutsPage,
+        ExercisesPage,
+        InfosPage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        Camera,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import {AlertController, LoadingController, NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public loadingCtrl: LoadingController, private alertCtrl: AlertController, private navCtrl: NavController, private camera: Camera) {
+        let loading = this.loadingCtrl.create();
 
-  }
+        loading.present();
+
+        setTimeout(() => {
+            loading.dismiss();
+        }, 500);
+    }
+    public onTakePhotoClicked(): void {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+            let alert = this.alertCtrl.create({
+                title: 'Image',
+                subTitle: base64Image,
+                buttons: ['Annuler']
+            });
+            alert.present();
+        }, (err) => {
+            alert(JSON.stringify(err));
+        });
+    }
 
 }
